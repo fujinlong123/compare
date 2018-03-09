2de8f88b6c8ca5537fc72bbf146179fa43c7c120
符金龙 <jinlong_fu@kingdee.com>
2018-03-09 17:31:29 +0800

    Signed-off-by: 符金龙 <jinlong_fu@kingdee.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     devtool: 'eval-source-map',
   
@@ -13,6 +13,16 @@ module.exports = {
       historyApiFallback: true,//不跳转
       inline: true//实时刷新
     } ,
+    plugins:[
+      new HtmlWebpackPlugin({
+        title:'rd平台',
+        template: __dirname +'/public/index.html', // 源模板文件
+        filename: __dirname +'/public/index.html', // 输出文件【注意：这里的根路径是module.exports.output.path】
+        showErrors: true,
+        inject: 'body',
+        chunks: ["common",'index']
+    })
+    ],
     module: {
             rules: [{
                test: /\.css$/,
